ba17e30efdf2ad63c5efd3d18171fe5cf171fa16
ImZacvin <103734636+ImZacvin@users.noreply.github.com>
2025-03-04 16:11:02 +0700

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-import time
-import requests
-import json
-from print_event import eventCalendar
-from access_the_token import load_tokens
-from check_exp import check_access_token
-from check_exp import refresh_access_token
-
